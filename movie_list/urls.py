f762c38b3ebe587963a989708225c17afb70b3d8
Akbar Ali <akbarali@Akbars-MacBook-Pro.local>
2024-09-15 12:52:26 +0530

    API view implemented

--- movie_list/urls.py
+++ movie_list/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('api/', views.list_movies, name='movie-list'),
+    path('classApi/', views.Movies.as_view(), name='movies'),
+
 ]
--- movie_list/views.py
+++ movie_list/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
+
+class Movies(APIView):
+    def get(self, request):
+        data = {
+            "name": 'akbar ali',
+            "age": 25,
+            "place": 'ekd'
+        }
+        return Response({'message': 'list_movies: this is api GET view', 'response': data})
+
+    def post(self, request):
+        return Response({'message': 'list_movies: this is api POST view'})
+
+
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def list_movies(request):
